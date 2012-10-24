481feba27efc9943aedb8de45ed3b6b2b43a94e5
Zoran Mijatovic <zmijatovic@alturabit.com>
2012-10-24 16:51:40 -0300

    Home page done, contact form not working yet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qaddy::Application.config.secret_token = '03fc53bd633932fe639c3795afa74f5490a697553eedc769b386226b4cfc3d8a4d7e8b30c7eb7bc1aede83f6b6b1134bf163c5affdd9e52cb8418c7b0c166953'
