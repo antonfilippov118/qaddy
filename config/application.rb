859065832584f5830bd96a1f92a57d6bfb259a63
Zoran Mijatovic <zmijatovic@alturabit.com>
2013-03-25 17:32:11 -0300

    Site email from config

--- config/application.rb
+++ config/application.rb
@@ -72,14 +72,14 @@ module Qaddy
     config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
     # mailer options
-    ActionMailer::Base.default(from: "noreply@getqaddy.com")
+    ActionMailer::Base.default(from: ENV['MAILER_DEFAULT_FROM'])
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = false
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address              => "smtp.sendgrid.net",
       :port                 => 587,
-      :domain               => 'getqaddy.com',
+      :domain               => ENV['SENDGRID_DOMAIN'],
       :user_name            => ENV['SENDGRID_USERNAME'],
       :password             => ENV['SENDGRID_PASSWORD'],
       :authentication       => :plain,
