6e63869e1b5902d6ffe552be8d3e8862e24872aa
Zoran Mijatovic <zmijatovic@alturabit.com>
2013-04-05 12:18:17 -0300

    Make session cookie secure only (ssl) for production env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,8 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Qaddy::Application.config.session_store :active_record_store, key: 'lacp_sess'
+Qaddy::Application.config.session_store :active_record_store,
+  key: 'lacp_sess',
+  secure: Rails.env == 'production'
+
 ActiveRecord::SessionStore.session_class = Session
